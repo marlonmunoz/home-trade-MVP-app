6a9c0c2bc29032330580a506a2b2b0c725622582
Marlon Munoz <marlon@marlonshade.com>
2025-10-26 04:22:58 -0400

    AuthContext.jsx created

--- client/hometrade_mvp/src/main.jsx
+++ client/hometrade_mvp/src/main.jsx
@@ -1,14 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './components/App.jsx'
-import { ThemeProvider } from './contexts/ThemeContext.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './components/App.jsx';
+import { ThemeProvider } from './contexts/ThemeContext.jsx';
+import { AuthProvider } from './contexts/AuthContext.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ThemeProvider>
-      <App />
+      <AuthProvider>
+        <App />
+      </AuthProvider>
     </ThemeProvider>
-  </StrictMode>,
-)
+  </StrictMode>
+);
 
